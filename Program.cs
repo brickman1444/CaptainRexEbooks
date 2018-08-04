87736ee7b98c87d0fd83eea88d44f117eff53380
Zac Gross <brickman1444@gmail.com>
2018-08-03 22:05:08 -0500

    Prototype markov chain

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿﻿using System;
+
+using Markov;
 
 namespace AppraisalBot
 {
@@ -6,7 +8,19 @@ namespace AppraisalBot
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var chain = new MarkovChain<string>(1);
+
+            chain.Add(new[] { "Once", "upon", "a", "time." });
+            chain.Add(new[] { "Once", "there", "was", "a", "pig." });
+            chain.Add(new[] { "There", "once", "was", "a", "man", "from", "Nantucket." });
+
+            var rand = new Random();
+
+            for (int i = 0; i < 10; i++)
+            {
+                var sentence = string.Join(" ", chain.Chain(rand));
+                Console.WriteLine(sentence);
+            }
         }
     }
 }
